700a510735436b831a3f9d11fba36a80d15072fb
Sammy Sloan <smileysammy3@gmail.com>
2020-06-13 19:40:46 -0600

    added server code

--- server.js
+++ server.js
@@ -1,1 +1,21 @@
+// import dependencies 
+var express = require("express");
 
+// create an "express" server
+var app = express();
+
+// define port where server will listen
+var PORT = process.env.PORT || 8080;
+
+// add middleware
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static("public"));
+
+// routes
+
+
+// start the server
+app.listen(PORT, function () {
+    console.log("Server is listening on http://localhost:" + PORT);
+});
